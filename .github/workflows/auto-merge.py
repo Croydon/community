957d6b7043061b8cad17f96e40d229c199767d4f
Croydon <git@cr0ydon.com>
2020-11-17 18:10:59 +0100

    test2

---
+++ .github/workflows/auto-merge.py
@@ -0,0 +1,11 @@
+from github import Github
+
+import os
+
+g = Github(os.getenv("GITHUB_TOKEN"))
+
+
+print(os.getenv("GITHUB_EVENT_NAME"))
+print(os.getenv("GITHUB_EVENT_PATH"))
+print(os.getenv("GITHUB_REF"))
+print(os.getenv("GITHUB_SHA"))
