b6664328e3aefe8513c1e57c1eb77795ebdd6387
Croydon <git@cr0ydon.com>
2020-11-17 20:06:16 +0100

    Update auto-merge.py

--- .github/workflows/auto-merge.py
+++ .github/workflows/auto-merge.py
@@ -9,3 +9,6 @@ print(os.getenv("GITHUB_EVENT_NAME"))
 print(os.getenv("GITHUB_EVENT_PATH"))
 print(os.getenv("GITHUB_REF"))
 print(os.getenv("GITHUB_SHA"))
+
+with open(os.getenv("GITHUB_EVENT_PATH"), mode="r") as payload:
+    print(payload.read())
